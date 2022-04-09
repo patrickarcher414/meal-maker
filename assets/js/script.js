2b8fc697b7b824719120f0d176bf89838bfef00e
Cameron <sicklecj@gmail.com>
2022-04-09 15:52:04 -0500

    createLiElement function to generate li elements for ingredient items

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var spoonKey = '129215e0949b4e2284c679c5b3665666'
 var form = document.querySelector('form')
 var addIng = document.querySelector('[name="ingredients"]')
 var ingList = []
-var createLi = document.createElement('li')
 var listContUlEl = document.getElementById('list-cont')
 
 function displayList() {
@@ -22,9 +21,21 @@ function addIngredient() {
         ingList.push(JSON.stringify(input.value))
         console.log(ingList)
         localStorage.setItem('ingredients', JSON.stringify(ingList))
+
+        //create a Li element
+        // make the innerText the input.value
+        // give the Li a unique ID
+        // give the Li a button el
+        //give the Li Btn element a event listener
+        //append the Btn to the Li
+        //append the Li to the list-cont Ul
+
+        createLiElement(input.value)
+
     } else {
         console.log('not a valid ingredient')
     }
+
     // var savedIng = localStorage.getItem('ingredients')
     // var displayIng = JSON.parse(savedIng)
     // console.log(displayIng + " this is pulled from local storage")
@@ -35,6 +46,14 @@ function addIngredient() {
     //     listContUlEl.appendChild(createLi)
     // }
 }
+
+function createLiElement() {
+    var listItemEl = document.createElement('li')
+    listItemEl.innerText = input.value
+    listItemEl.setAttribute("id", input.value)
+    listContUlEl.appendChild(listItemEl)
+}
+
 
 function handleSubmit(ev) {
     ev.preventDefault()
