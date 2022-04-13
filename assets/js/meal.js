fdc9054f1ea476e9fdb3954c79c6a4d942dd7842
patrickarcher414 <patrickarcher414@gmail.com>
2022-04-12 19:54:10 -0500

    fetch working

--- assets/js/meal.js
+++ assets/js/meal.js
@@ -1,14 +1,16 @@
-var spoonKey = '129215e0949b4e2284c679c5b3665666'
 
+var edamamKey = 'ad1b23b5a41114ae64b13d36bb2dceda'
+var edamamId = '62eb7f7d'
+var spoonKey = '129215e0949b4e2284c679c5b3665666'
 var savedIng = localStorage.getItem('ingredients')
 var ingredients = JSON.parse(savedIng) || []
 
 // fetch requests to the API Spoonacular and Edamam
 // need to add a variable to the value of ingredients parameter that's equal to savedIng
 function getSpoonData() {
-    fetch ('https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + ingredients.join(',') + '&ranking=1&ignorePantry=true') 
+    fetch ('https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + ingredients.join(',') + '&ranking=1&ignorePantry=true&apiKey=' + spoonKey ) 
         .then(function(response) {
-            response.JSON
+            return response.JSON
         })
         .then(function(data) {
             console.log(data)
@@ -17,7 +19,7 @@ function getSpoonData() {
 
 
 function getEdamamData() {
-    fetch ('https://api.edamam.com/api/recipes/v2?q=' + + '&app_id=' + + '&app_key=') 
+    fetch ('https://api.edamam.com/api/recipes/v2?q=' + ingredients.join(',') + '&app_id=' + + '&app_key=') 
         .then(function(response) { 
             response.JSON
         })
@@ -26,13 +28,6 @@ function getEdamamData() {
         })
 }
 
-
-
-
-
-
-
-//this call is not being used right now but could. See earlier comment for details.
-// displayList()
+getSpoonData()
 
 
